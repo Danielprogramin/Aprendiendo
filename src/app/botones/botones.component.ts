30816620b3e727e86ce1db3d0c535c3253f08162
Daniel Ortiz <andresvertel2021@gmail.com>
2023-09-28 08:43:10 -0500

    Actualizado

---
+++ src/app/botones/botones.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-botones',
+  templateUrl: './botones.component.html',
+  styleUrls: ['./botones.component.css']
+})
+export class BotonesComponent {
+
+ src:string = "https://images.pexels.com/photos/18397478/pexels-photo-18397478/free-photo-of-coche-vehiculo-vintage-lujo.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load";
+
+}
---
+++ src/app/formulario/formulario.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-formulario',
+  templateUrl: './formulario.component.html',
+  styleUrls: ['./formulario.component.css']
+})
+export class FormularioComponent {
+
+  mostar_en_consola(name: string){
+    console.log(name);
+  }
+
+}
